038fe877ab944cd4af845ecb2d99091de0182f1e
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-25 09:09:55 -0700

    create: controllers for warehouse

---
+++ controllers/warehouse.js
@@ -0,0 +1,29 @@
+import initKnex from "knex";
+import configuration from "../knexfile.js";
+const knex = initKnex(configuration);
+
+const getWarehouses = async (req, res) => {
+  try {
+    const data = await knex("warehouses");
+    const formattedData = data.map((warehouse) => {
+      return {
+        id: warehouse.id,
+        warehouse_name: warehouse.warehouse_name,
+        address: warehouse.address,
+        city: warehouse.city,
+        country: warehouse.country,
+        contact_name: warehouse.contact_name,
+        contact_position: warehouse.contact_position,
+        contact_phone: warehouse.contact_phone,
+        contact_email: warehouse.contact_email,
+      };
+    });
+    res.status(200).json(formattedData);
+  } catch (err) {
+    res.status(400).send(`Error retrieving Warehouses: ${err}`);
+  }
+};
+
+const getOneWarehouse = async (req, res) => {};
+
+export { getWarehouses, getOneWarehouse };
