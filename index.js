5e0e33e4d776a85a4029f8a46da1cb0d26d575f6
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-24 19:17:23 -0700

    fix: route to api/warehouses

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ app.use(express.json());
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("This is a homePage for In-Stock Project API, please make a request!");
+  res.send(
+    "This is a homePage for In-Stock Project API, please make a request!"
+  );
 });
 
-app.use("/warehouse", warehouseRoutes);
+app.use("/api/warehouses", warehouseRoutes);
 
 app.listen(PORT, () => {
   console.log("App is running on port ", PORT);
