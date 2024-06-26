97cf92315d911be68d968524e23612e9e14950bb
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-26 08:27:16 -0700

    delete: wrong route created

--- routes/inventory.js
+++ routes/inventory.js
@@ -2,10 +2,8 @@ import express from "express";
 import * as inventoryControllers from "../controllers/inventory.js";
 const router = express.Router();
 
-router.route("/")
-    .get(inventoryControllers.getInventory)
+router.route("/").get(inventoryControllers.getInventory);
 
-router.route("/:id")
-    .get(inventoryControllers.getOneInventory)
+router.route("/:id").get(inventoryControllers.getOneInventory);
 
 export default router;
