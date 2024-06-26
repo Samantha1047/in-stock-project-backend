3623684a758da68812785588327f2fd0316afa4f
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-26 08:26:49 -0700

    add: new route to :id/inventories

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -2,7 +2,17 @@ import express from "express";
 import * as warehouseControllers from "../controllers/warehouse.js";
 const router = express.Router();
 
-router.route("/").get(warehouseControllers.getWarehouses).post(warehouseControllers.postOneWarehouse);
-router.route("/:id").get(warehouseControllers.getOneWarehouse).put(warehouseControllers.editOneWarehouse);
+router
+  .route("/")
+  .get(warehouseControllers.getWarehouses)
+  .post(warehouseControllers.postOneWarehouse);
+router
+  .route("/:id")
+  .get(warehouseControllers.getOneWarehouse)
+  .put(warehouseControllers.editOneWarehouse);
+
+router
+  .route("/:id/inventories")
+  .get(warehouseControllers.getInventoryByWarehouseId);
 
 export default router;
